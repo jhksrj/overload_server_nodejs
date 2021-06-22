d5661eaa256d82de9a51e4c0b121c3c704e4a9e8
HyunkiJin <hyunki.jin01@gmail.com>
2021-06-22 17:28:40 +0900

    CORS issue 0

--- main.js
+++ main.js
@@ -10,6 +10,14 @@ const morgan = require('morgan');
 const user = require('./routes/user');
 const evalbootpay = require('./routes/evalBootpay');   // 부트 페이 결제 검증 
 
+//CORS 이슈 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(morgan('dev'));
